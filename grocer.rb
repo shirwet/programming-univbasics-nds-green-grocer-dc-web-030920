dba9fee836d920df8de8234fac32a1c6e43967d6
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 13:39:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,12 +45,21 @@ def apply_coupons(cart, coupons)
   if cart_item && cart_item[:count] >= coupons[counter][:num]
     if cart_item_with_coupon
       cart_item_with_coupon[:count] += coupons[counter][:num]
-      cart_item[]
-      
+      cart_item[:count] -= coupons[counter][:num]
+    else
+      cart_item_with_coupon {
+        :item => couponed_item_name,
+        :price => coupons[counter][:cost] / coupons[counter][:cost],
+        :count => coupons[counter][:num],
+        :clearance => cart_item[:clearance]
+      }
+      cart << cart_item_with_coupon
+      cart_item[counter] -= coupons[counter][:num]
     end
   end
   counter +=1
 end
+cart
 end
 
 def apply_clearance(cart)
