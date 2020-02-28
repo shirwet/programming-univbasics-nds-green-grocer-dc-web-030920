abf85b5cd04b44f039c74f8b372a3049fb78a180
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 13:43:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -55,7 +55,7 @@ def apply_coupons(cart, coupons)
         
       }
       cart << cart_item_with_coupon
-      cart_item[counter] -= coupons[counter][:num]
+      cart_item[:count] -= coupons[counter][:num]
     end
   end
   counter +=1
