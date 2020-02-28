de6a7a53c7d486dd60cd44c774d8668160bdb2e8
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 13:23:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,20 +1,48 @@
-def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  def find_item_by_name_in_collection (name, collection)
+    
+    counter = 0 
+    while counter < collection.length
+   if collection[counter][:item] == name
+     return collection[counter]
+   end
+    counter +=1
+  end
 end
 
-def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+
+def consolidate_cart (cart)
+  new_cart = []
+  
+  counter = 0
+  
+  while counter < cart.length 
+new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
+if new_cart_item != nil 
+  new_cart_item [:count] += 1 
+else 
+ new_cart_item = {
+   :item => cart[counter][:item],
+   :price => cart[counter][:price],
+   :clearance => cart[counter][:clearance],
+   :count => 1
+ }
+    new_cart << new_cart_item
+end
+  counter +=1
+end
+new_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length 
+  cart_item = find_item_by_name_in_collection(coupons[counter][:item],cart)
+  couponed_item_name = "#{"
+  counter +=1
+end
 end
 
 def apply_clearance(cart)
--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -55,3 +55,55 @@ coupons.each do |coupon|
 end
 
 puts "Your total is #{checkout(cart: cart, coupons: coupons)}"
+
+def find_item_by_name_in_collection (string, array)
+  
+end
+
+def consolidate_cart(cart:[])
+consolidated_cart = {}
+
+    cart.each do |item|
+     item_name = item.keys[0]
+
+      consolidated_cart[item_name] = item.values[0]
+
+      if consolidated_cart[item_name][:count] 
+       consolidated_cart[item_name][:count] += 1
+     else
+       consolidated_cart[item_name][:count] = 1
+     end
+   end
+
+    consolidated_cart
+end
+
+def apply_coupons(cart:[], coupons:[])
+   # code here		   coupons.each do |coupon|
+     coupon_name = coupon[:item]
+     coupon_item_num = coupon[:num]
+     cart_item = cart[coupon_name]
+
+      next if cart_item.nil? || cart_item[:count] < coupon_item_num
+
+      cart_item[:count] -= coupon_item_num
+
+      coupon_in_cart = cart["#{coupon_name} W/COUPON"]
+
+      if coupon_in_cart
+       coupon_in_cart[:count] += 1
+     else
+       cart["#{coupon_name} W/COUPON"] = { 
+         price: coupon[:cost], 
+         clearance: cart_item[:clearance], 
+         count: 1
+       }
+     end
+   end
+
+    cart
+  end
+  
+
+
+
