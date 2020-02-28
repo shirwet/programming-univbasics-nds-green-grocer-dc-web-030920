68d8a214080ea84b279db6535e052a68509df4bd
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 13:25:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,8 @@ def apply_coupons(cart, coupons)
   counter = 0 
   while counter < coupons.length 
   cart_item = find_item_by_name_in_collection(coupons[counter][:item],cart)
-  couponed_item_name = "#{"
+  couponed_item_name = "#{coupons[counter][:item]} W/COUPON"
+  cart_item_with_coupon = 
   counter +=1
 end
 end
