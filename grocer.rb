7c069e647ab9993dcb568f99264b86aae5d646bb
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 14:03:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -72,7 +72,7 @@ while counter < cart.length
 
 if cart[counter][:clearance]
   new_cart_item_clearance = cart_item(clearance[counter][:item],cart)
-cart[counter][:price] = cart
+cart[counter][:price] = cart[counter][:price] - ()
   return clearance_item
 else
   return FALSE
