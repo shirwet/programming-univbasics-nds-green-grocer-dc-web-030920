b97b1b835f6435976f13b37ad033f8ff8e69258d
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-28 14:20:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -83,7 +83,7 @@ end
 def checkout(cart, coupons)
  new_consolidate_cart = consolidate_cart (cart)
  couponed_cart = apply_coupons(new_consolidate_cart)
- clearanced_cart = appl
+ clearanced_cart = apply_clearance(couponed_cart)
   counter = 0
   total = 0
 while counter < coupons.length
